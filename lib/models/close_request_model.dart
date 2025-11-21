e4e93dae2155db0d32398f87d145d0fde0fbe452
Sanjai <sanjaipaulraj03@gmail.com>
2025-11-21 13:11:07 +0530

    Open and Close fn upd

--- lib/models/close_request_model.dart
+++ lib/models/close_request_model.dart
@@ -5,10 +5,8 @@ part 'close_request_model.g.dart';
 @JsonSerializable()
 class CloseRequestModel {
   String actionType;
-  // String? symbol;
   String? positionId;
 
-  // CloseRequestModel({required this.actionType, required this.symbol});
   CloseRequestModel({required this.actionType, required this.positionId});
 
   factory CloseRequestModel.fromJson(Map<String, dynamic> json) => _$CloseRequestModelFromJson(json);
